3678e15b936ada1fede61e581feaa267fd3a12bd
yuixixixixi <yendra.paundra@gmail.com>
2025-05-06 10:45:45 +0700

    Persegi panjang

---
+++ point.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class PersegiPanjang
+{
+    public: //akses modifier
+    int panjang,lebar;
+
+    public:
+
+    void input()
+    {
+        cout << "masukkan panjang : ";
+        cin >> panjang;
+        cout << "masukkan lebar : ";
+        cin >> lebar;
+
+    }
+    int luasPp(int a, int b)
+    {
+        return a*b;
+    }
+    lingkaran 0l;
+};
+class Lingkaran
+{
+    public:
+    int jarijari;
+
+    voidinput()
+    {
+        cout << "masukkan jari-jari : ";
+        cin >> jari-jari;
+    }
+
+    int luasLingkaran(int r)
+    {
+        return 3.14*r*r;
+    }
+}
