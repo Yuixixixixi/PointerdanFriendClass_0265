2dd4f5a340a29d3b997fcbe64287781b9b7f1694
yuixixixixi <yendra.paundra@gmail.com>
2025-05-06 11:37:43 +0700

    class mahasiswa(point2)

---
+++ pointerdua.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa
+{
+    public :
+    int nim;
+    void showNim()
+    {
+        cout << "No induk = " << endl;
+    }
+};
+int main()
+{
+    mahasiswa *mhs = new mahasiswa{1}; // pointer objek mhs
+    mhs->nim = 2;
+    mhs->showNim();
+    delete mhs;
+    return 0;
+}
